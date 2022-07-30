93f2afb3500630129f6758ed8a268de020bdcb72
Marcos Cavalcante <88354918+marcoscvlc@users.noreply.github.com>
2022-07-30 19:21:08 -0300

    Add files via upload

---
+++ DESAFIOS - DIO Typescript/desafio1 - RESOLVIDO.ts
@@ -0,0 +1,15 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+/*
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+
+*/
+
+//solução
+
+let employee = {
+    code:10,
+    name: 'Jhon'
+};
